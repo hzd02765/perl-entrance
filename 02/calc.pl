12ca0bab4f8682f8b194a9704636eacfb97c4510
tomoyuki@sakura <tomoyuki@www1269ue.sakura.ne.jp>
2015-12-03 12:04:43 +0900

    $B$A$g$C$H$d$C$?(B

--- 02/calc.pl
+++ 02/calc.pl
@@ -3,50 +3,55 @@
 use strict;
 use warnings;
 
+# 練習問題 (calc.pl)
+
+# 標準入力<STDIN>から0以外の整数を2つ読み込み,
+# それらを四則演算(+, -, *, /)した結果を
+# 上の例のように出力するスクリプト calc.plを作成しよう
+
 my $foo;
 my $bar;
 
 print 'input > ';
-#$foo = <STDIN>;
-$foo = 10;
+$foo = <STDIN>;
+
 print 'input > ';
-#$bar = <STDIN>;
-$bar = 3;
+$bar = <STDIN>;
 
 chomp $foo;
 chomp $bar;
 
-print '$foo : ', $foo, "\n";
-print '$bar : ', $bar, "\n";
+#print '$foo : ', $foo, "\n";
+#print '$bar : ', $bar, "\n";
 
 my $res;
 
-print "たし算 : ";
+print "たし算 : "."\n";
 $res = $foo + $bar;
-print $res, "\n";
+#print $res, "\n";
 print "$foo + $bar = $res\n";
 
-print "引き算 : ";
+print "引き算 : "."\n";
 $res = $foo - $bar;
-print $res, "\n";
+#print $res, "\n";
 print "$foo - $bar = $res\n";
 
-print "かけ算 : ";
+print "かけ算 : "."\n";
 $res = $foo * $bar;
-print $res, "\n";
+#print $res, "\n";
 print "$foo * $bar = $res\n";
 
-print "割り算 : ";
+print "割り算 : "."\n";
 $res = $foo / $bar;
-print $res, "\n";
+#print $res, "\n";
 print "$foo / $bar = $res\n";
 
-print "あまり : ";
+print "あまり : "."\n";
 $res = $foo % $bar;
-print $res, "\n";
+#print $res, "\n";
 print "$foo % $bar = $res\n";
 
-print "べき乗 : ";
+print "べき乗 : "."\n";
 $res = $foo ** $bar;
-print $res, "\n";
+#print $res, "\n";
 print "$foo ** $bar = $res\n";
--- 02/profile.pl
+++ 02/profile.pl
@@ -1,19 +1,18 @@
 #!/usr/bin/env perl
 
+# Œµ–§‚È‘®‚ğ’è‚ß‚½‚è, –¢’è‹`‚Ì•Ï”‚ğŒx‚·‚é‚Æ‚¢‚Á‚½Œø‰Ê‚ª‚ ‚è‚Ü‚·
 use strict;
+# –]‚Ü‚µ‚­‚È‚¢‹Lq‚ğŒx‚µ‚Ä‚­‚ê‚éŒø‰Ê‚ª‚ ‚è‚Ü‚· 
 use warnings;
-
-my $hoge = 1;
-print "$hoge\n";
-print '$hoge\n';
-print "\n";
+use feature ':5.10';
 
 my $name;
 my $age;
 my $height;
 
 $name = "sakai tomoyuki";
-$age = 36;
+# $age = 36;
+$age = 37;
 $height = 167.8;
 
 print 'name : ', "$name\n";
